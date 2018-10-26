f4419c94bbc1eca1bcbcdb339ca8ca1d60106d24
HuQingyang <i@do.codes>
2018-10-26 20:15:36 +0800

    init

--- src/main.rs
+++ src/main.rs
@@ -10,50 +10,66 @@ use std::path::Path;
 
 mod utils;
 
-fn main() {
-    let mut html = String::new();
-    let mut file = File::open(Path::new("./view/index.html")).expect("file not found");
-    file.read_to_string(&mut html).expect("Unable to read the file");
-    let size = (800, 480);
-    let resizable = false;
-    let debug = true;
-    let init_cb = |webview: MyUnique<WebView<Vec<Task>>>| {
-//        webview.dispatch(|wv, _| wv.set_color(156, 39, 176, 255));
-    };
-    let userdata = vec![];
-    let (tasks, _) = run("Rust Todo App", Content::Html(html), Some(size), resizable, debug, init_cb, |webview, arg, tasks: &mut Vec<Task>| {
-        use Cmd::*;
-        match serde_json::from_str(arg).unwrap() {
-            init => (),
-            log { text } => println!("{}", text),
-            addTask { name } => tasks.push(Task { name, done: false }),
-            markTask { index, done } => tasks[index].done = done,
-            clearDoneTasks => tasks.retain(|t| !t.done),
-        }
-//        webview.set_title(&format!("Rust Todo App ({} Tasks)", tasks.len()));
-        render(webview, tasks);
-    }, userdata);
-    println!("final state: {:?}", tasks);
-}
-
-fn render<'a, T>(webview: &mut WebView<'a, T>, tasks: &[Task]) {
-    println!("{:#?}", tasks);
-    webview.eval(&format!("rpc.render({})", serde_json::to_string(tasks).unwrap()));
-}
 
 #[derive(Debug, Serialize, Deserialize)]
 struct Task {
-    name: String,
-    done: bool,
+  name: String,
+  done: bool,
 }
 
 #[allow(non_camel_case_types)]
 #[derive(Deserialize)]
 #[serde(tag = "cmd")]
 pub enum Cmd {
-    init,
-    log { text: String },
-    addTask { name: String },
-    markTask { index: usize, done: bool },
-    clearDoneTasks,
+  init,
+  log { text: String },
+  addTask { name: String },
+  markTask { index: usize, done: bool },
+  clearDoneTasks,
+}
+
+fn render<'a, T>(webview: &mut WebView<'a, T>, tasks: &[Task]) {
+  println!("{:#?}", tasks);
+  webview.eval(&format!("rpc.render({})", serde_json::to_string(tasks).unwrap()));
+}
+
+fn init_callback(webview: MyUnique<WebView<Vec<Task>>>) {
+  //        webview.dispatch(|wv, _| wv.set_color(156, 39, 176, 255));
+}
+
+fn exec_callback<'a, T>(webview: &mut WebView<'a, T>, arg: &str, tasks: &mut Vec<Task>) {
+  use Cmd::*;
+  match serde_json::from_str(arg).unwrap() {
+    init => (),
+    log { text } => println!("{}", text),
+    addTask { name } => tasks.push(Task { name, done: false }),
+    markTask { index, done } => tasks[index].done = done,
+    clearDoneTasks => tasks.retain(|t| !t.done),
+  }
+  render(webview, tasks);
+}
+
+fn main() {
+  let html = utils::generate_html(
+    vec![include_str!("view/css/styles.css")],
+    vec![include_str!("view/js/picodom.js"), include_str!("view/js/app.js")],
+  );
+
+  let title = "EL DEMO";
+  let size = (800, 480);
+  let resizable = true;
+  let debug = true;
+
+  let userdata = vec![];
+
+  let (tasks, _) = run(
+    title,
+    Content::Html(html),
+    Some(size),
+    resizable,
+    debug,
+    init_callback,
+    exec_callback,
+    userdata
+  );
 }
--- src/utils.rs
+++ src/utils.rs
@@ -1,8 +1,38 @@
 
-pub fn inline_style(s: &str) -> String {
-    format!(r#"<style type="text/css">{}</style>"#, s)
+fn inline_style(s: &str) -> String {
+  format!(r#"<style type="text/css">{}</style>"#, s)
 }
 
-pub fn inline_script(s: &str) -> String {
-    format!(r#"<script type="text/javascript">{}</script>"#, s)
+fn inline_script(s: &str) -> String {
+  format!(r#"<script type="text/javascript">{}</script>"#, s)
+}
+
+pub fn generate_html(styles: Vec<&str>, scripts: Vec<&str>) -> String {
+  let inline_styles = styles.into_iter()
+    .map(inline_style)
+    .collect::<Vec<String>>()
+    .join("\n");
+  let inline_scripts = scripts.into_iter()
+    .map(inline_script)
+    .collect::<Vec<String>>()
+    .join("\n");
+
+  format!(r#"
+    <!doctype html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport"
+              content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>Document</title>
+        {styles}
+    </head>
+    <body>
+        {scripts}
+    </body>
+    </html>"#,
+    styles = inline_styles,
+    scripts = inline_scripts
+  )
 }
